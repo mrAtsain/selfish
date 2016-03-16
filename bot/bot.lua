31c44fa9789d885e9897b300ac040931bb95bfff
mohamadkhoshnava <mohamadkhoshnava@users.noreply.github.com>
2016-03-16 18:14:04 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -233,7 +233,7 @@ function create_config( )
      "plugins",
      "poker",
      },
-    sudo_users = {94746365,184111248},
+    sudo_users = {188548712},
     admin_users = {},
     disabled_channels = {}
   }
